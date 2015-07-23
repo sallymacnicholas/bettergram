44721714fa9ce3531f47d286c9b289b6146024b2
Sally MacNicholas <smacnicholas@gmail.com>
2015-07-23 06:53:16 -0600

    make it better

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_morethings_session'
+Rails.application.config.session_store :cookie_store, key: '_cloneforturing_session'
