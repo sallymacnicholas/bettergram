54e4c73234410bf45c07565c8267d75795ee0753
Sally MacNicholas <smacnicholas@gmail.com>
2015-09-08 14:39:49 -0600

    change new session redirect path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
     if @user
       session[:user_id] = @user.id
-      redirect_to profile_path
+      redirect_to dashboard_feed_path
     else
       redirect_to root_path
     end
