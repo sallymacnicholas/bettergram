e86831cf9d1edba6cc3c41185df90cfa18b1e9a9
Sally MacNicholas <smacnicholas@gmail.com>
2016-05-16 20:20:24 -0500

    committing new error pages

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
 
   resources :profile, only: [:show]
   resources :stats, only: [:index]
+
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
 end
